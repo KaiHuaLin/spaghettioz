1d734d016b94d58c181665ce22a3ad202ac2daa7
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2021-02-08 18:44:29 -0600

    changed dist location

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAnkhyQIw6Du-38AQ9EqGhJgEGJJsbOBQQ",
+    authDomain: "spaghettio.firebaseapp.com",
+    projectId: "spaghettio",
+    storageBucket: "spaghettio.appspot.com",
+    messagingSenderId: "534240799840",
+    appId: "1:534240799840:web:2e2d280745500c9fe12705"
+  }
 };
