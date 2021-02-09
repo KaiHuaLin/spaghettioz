7f1c66a38acde5e8ca62634ce9b52883209bc419
kaihualin <kl2cb@mail.missouri.edu>
2021-02-08 18:37:14 -0600

    initialized recipe api

--- back-end/functions/src/app.js
+++ back-end/functions/src/app.js
@@ -8,6 +8,12 @@ const express = require("express");
 
 const router = new express.Router();
 
+
+const recipeRouter = require("./routes/recipe");
+router.use("/recipes", recipeRouter);
+
+
+
 router.route("/").get((req, res) => {
     return res.status(200).send("hello world from api index");
 });
---
+++ back-end/functions/src/routes/recipe.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = new express.Router();
+
+router.route("/").get((req, res) => {
+    // retreive recipes from spoon
+    return res.status(200).send("recipes api");
+})
+
+module.exports = router;
