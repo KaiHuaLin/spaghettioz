bebf07c4b297d9e6997bab08af271cc37b4e3827
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2021-02-17 16:22:47 -0600

    updated homepage

--- front-end/src/app/pages/login/login.component.ts
+++ front-end/src/app/pages/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
 
   loginFormGroup: FormGroup;
   registerFormGroup: FormGroup;
+  user: any;
 
   constructor(private http: HttpClient) { 
     this.loginFormGroup = new FormGroup(
@@ -34,19 +35,34 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-register(){
-   if(this.registerFormGroup.valid){
-    const body = { email: this.registerFormGroup.get("email").value,
-                    password:  this.registerFormGroup.get("password").value,
-                    displayName: this.registerFormGroup.get("name").value};
-    console.log(body);
-    this.http.post("https://us-central1-spaghettio.cloudfunctions.net/api/users/", body);
+  register(){
+    if(this.registerFormGroup.valid){
+     const body = { email: this.registerFormGroup.get("email").value,
+                     password:  this.registerFormGroup.get("password").value,
+                     displayName: this.registerFormGroup.get("name").value};
+     console.log(body);
+     this.http.post("https://us-central1-spaghettio.cloudfunctions.net/api/users/", body).subscribe(data =>  
+     console.log(data)
+     
+  );
+    }
+     
    }
-    
-  }
-
-  login(){
-
-  }
+ 
+   login(){
+     if(this.loginFormGroup.valid){
+       const body = { email: this.loginFormGroup.get("email").value,
+                     password:  this.loginFormGroup.get("password").value};
+     console.log(body);
+     this.http.post("https://us-central1-spaghettio.cloudfunctions.net/api/users/login/", body, {responseType: 'text' }).subscribe(data =>  {
+       if(data){
+         this.user = data;
+         console.log(this.user);
+       } 
+       //this.router.navigate(['favorites'])
+     });
+     
+     }
+    }
 
 }
