c14ba55cd64d4acb445691fbffe4ee9e9e45d757
kaihualin <kl2cb@mail.missouri.edu>
2021-02-18 15:49:40 -0600

    merge fixed

--- front-end/src/app/pages/login/login.component.ts
+++ front-end/src/app/pages/login/login.component.ts
@@ -20,37 +20,11 @@ export class LoginComponent implements OnInit {
       },
       Validators.required
     );
-     
-    // this.registerFormGroup = new FormGroup(
-    //   {
-    //     name: new FormControl(""),
-    //     email: new FormControl("", Validators.email),
-    //     password: new FormControl("", Validators.minLength(8))
-    //   },
-    //   Validators.required
-    // );
   }
 
   ngOnInit(): void {
   }
-
-  // async register() {
-  //   if (this.registerFormGroup.valid) {
-  //     const email = this.registerFormGroup.get("email").value;
-  //     const password = this.registerFormGroup.get("password").value;
-  //     const displayName = this.registerFormGroup.get("name").value;
-
-  //     try {
-  //       const user = await this.AuthService.create_user(email, password, displayName);
-  //       console.log(user);
-
-  //       // user will be logged in once the user is created
-  //     }
-  //     catch (error) {
-  //       console.log("Error: " + error);
-  //     }
-  //   }
-  // }
+  
 
   async login() {
     const email = this.loginFormGroup.get("email").value;
