494db5c8b9a80a70f7f7820c65be708b1116d640
Royal Sanders <royaljsanders@gmail.com>
2021-05-03 13:01:42 -0500

    added recipe tests, get recipe by query test is not finished

--- front-end/src/app/service/db/recipe-preview.service.spec.ts
+++ front-end/src/app/service/db/recipe-preview.service.spec.ts
@@ -22,4 +22,24 @@ describe('RecipePreviewService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('create recipe & get by id & delete', async () => {
+    //create a test recipe in fire store, then check to see if it's in there.
+    await service.create_recipe({id:'555', image:'https://www.awesomeinventions.com/wp-content/uploads/2020/03/Ikiru-the-Cat.jpg', title:'blep cat'})
+    // this doesn't delete recipe 555, but it works if 555 is manually deleted
+    let recipe = service.get_recipe_by_id('555');
+    expect(recipe).toBeTruthy();
+    console.log(recipe);
+  });
+
+  it('get recipe by id', () => {
+    // use id null = failure;
+    // let recipe = service.get_recipe_by_id("abcdef");
+    // expect(recipe).toBeNull();
+    
+    // use id 104000 = 'Eggplant Parmesan'
+    let recipe = service.get_recipe_by_id('104000');
+    expect(recipe).toBeTruthy();
+    console.log(recipe);
+  });
 });
--- front-end/src/app/service/recipe/recipe.service.spec.ts
+++ front-end/src/app/service/recipe/recipe.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { Query } from '../../models/Query';
 
 import { RecipeService } from './recipe.service';
 
@@ -15,5 +16,30 @@ describe('RecipeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('get recipe by id', () => {
+    // use id null = failure;
+    // let recipe = service.get_recipe_by_id("abcdef");
+    // expect(recipe).toBeNull();
+    
+    // use id 104000 = 'Eggplant Parmesan'
+    let recipe = service.get_recipe_by_id('104000');
+    expect(recipe).toBeTruthy();
+    console.log(recipe);
+  });
+
+  it('get recipe by query', async () => {
+    let ingredientList = "beans,rice,tomato";
+
+    const query: Query = {
+      // includeIngredients: ingredientList, 
+      ingredients: ingredientList,
+    }
+
+    const recipes = await service.get_recipe_by_query(query);
+    // console.log('get recipe by query test');
+    // console.log(recipes);
+    expect(recipes.length).toBeGreaterThan(0);
   });
 });
